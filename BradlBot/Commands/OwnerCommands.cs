799dafe70e9077a01e852422d81e8ee70d1d76b0
Bradley Eaton <thebradad1111@users.noreply.github.com>
2019-11-11 22:07:14 +0000

    Fixed shutdown and restart commands

--- BradlBot/Commands/OwnerCommands.cs
+++ BradlBot/Commands/OwnerCommands.cs
@@ -11,29 +11,12 @@ namespace BradlBot.Commands
         [RequireOwner]
         [Command("shutdown")]
         [Description("Turns the bot off")]
-        public async Task Shutdown(CommandContext ctx,
-            [Description("Turns the program off completely if y")] string hardShutdown = "n")
+        public async Task Shutdown(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
-            switch (hardShutdown.ToLower().Trim())
-            {
-                    case "hard":
-                    case "force":
-                    case "y":
-                        CommandsCommon.RespondWithSuccess(ctx,"Shutting down forcefully");
-                        await Task.Delay(100);
-                        Environment.Exit(0);
-                        break;
-                    case "n":
-                        CommandsCommon.RespondWithSuccess(ctx,
-                            $"Shutting down.\n {ctx.User.Mention} use backend console to turn back on.");
-                        await ctx.Client.DisconnectAsync();
-                        Console.ReadKey();
-                        Program.Main(null);
-                        break;
-                    default:
-                        throw new ArgumentException($"Only y or n accepted after {ctx.Command.Name}");
-            }
+            CommandsCommon.RespondWithSuccess(ctx,"Shutting down");
+            await Task.Delay(100);
+            Environment.Exit(0);
         }
 
         [RequireOwner]
@@ -46,7 +29,7 @@ namespace BradlBot.Commands
             CommandsCommon.RespondWithWarning(ctx,"Restarting");
             await ctx.Client.DisconnectAsync();
             await Task.Delay(1000);
-            Program.Main(null);
+            Program.Main(new string[0]);
         }
     }
 }
